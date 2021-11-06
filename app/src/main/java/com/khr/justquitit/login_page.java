42c068e2256f38de26d9a39bcb1645d5930e3d6a
jeeyueqian <jeeyueqian99@gmail.com>
2021-11-07 04:39:29 +0800

    hello

--- app/src/main/java/com/khr/justquitit/login_page.java
+++ app/src/main/java/com/khr/justquitit/login_page.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class login_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.first_page);
+        setContentView(R.layout.login_page);
     }
 }
