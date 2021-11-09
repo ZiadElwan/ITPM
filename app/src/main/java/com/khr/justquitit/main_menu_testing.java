9b3249ac8e2d86dae86e6467074c6947677d41d2
jeeyueqian <jeeyueqian99@gmail.com>
2021-11-10 04:27:05 +0800

    yueqian

--- app/src/main/java/com/khr/justquitit/main_menu_testing.java
+++ app/src/main/java/com/khr/justquitit/main_menu_testing.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class main_menu_testing extends AppCompatActivity {
+    private Button btnLogout;
     Button btnFeedback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +18,29 @@ public class main_menu_testing extends AppCompatActivity {
         setContentView(R.layout.main_menu_testing);
 
         btnFeedback = findViewById(R.id.btn_feedback_link);
+        btnLogout = findViewById(R.id.btn_logout);
 
         btnFeedback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(main_menu_testing.this, user_feedback_page.class);
                 startActivity(intent);
+
+
             }
         });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(main_menu_testing.this, login_page.class));
+            }
+        });
+
+
+
+
     }
 
 
