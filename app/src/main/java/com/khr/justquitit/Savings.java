1840aa5b372c1c78786d4b61db323e9e8527aec2
Kahar <kaharmuslim12@gmail.com>
2021-11-18 07:54:07 +0800

    saving button initialized

--- app/src/main/java/com/khr/justquitit/Savings.java
+++ app/src/main/java/com/khr/justquitit/Savings.java
@@ -1,11 +1,20 @@
 package com.khr.justquitit;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.khr.justquitit.adapter.SavingHistoryAdapter;
 import com.khr.justquitit.utils.SpacingItemDecoration;
 
@@ -13,6 +22,7 @@ import java.util.ArrayList;
 
 public class Savings extends AppCompatActivity {
 
+    ImageButton imgBtn;
     private RecyclerView recyclerView;
     private SavingHistoryAdapter savingAdapter;
     private ArrayList<SavingHistory> savingArray;
@@ -22,6 +32,37 @@ public class Savings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savings);
+
+        imgBtn = findViewById(R.id.imgBtn_saving);
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText addSaving = new EditText(view.getContext());
+                AlertDialog.Builder addSavingDialog = new AlertDialog.Builder(view.getContext());
+                addSavingDialog.setTitle("Add Saving");
+                addSavingDialog.setMessage("Enter amount(RM) you want to save");
+                addSavingDialog.setView(addSaving);
+                addSavingDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                addSavingDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String addedSaving = addSaving.getText().toString();
+
+                        //add the action later
+                    }
+                });
+
+                addSavingDialog.create().show();
+
+            }
+        });
 
         InitializeCardView();
     }
