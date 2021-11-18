777232c71b57b3b0a96bc30b5abd3f3d1a95904a
Kahar <kaharmuslim12@gmail.com>
2021-11-18 11:19:29 +0800

    saving nav bar added

--- app/src/main/java/com/khr/justquitit/Savings.java
+++ app/src/main/java/com/khr/justquitit/Savings.java
@@ -1,20 +1,27 @@
 package com.khr.justquitit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.khr.justquitit.adapter.SavingHistoryAdapter;
 import com.khr.justquitit.utils.SpacingItemDecoration;
 
@@ -22,19 +29,82 @@ import java.util.ArrayList;
 
 public class Savings extends AppCompatActivity {
 
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     ImageButton imgBtn;
     private RecyclerView recyclerView;
     private SavingHistoryAdapter savingAdapter;
     private ArrayList<SavingHistory> savingArray;
     private static final int VERTICAL_ITEM_SPACE = 48;
 
+    FirebaseAuth fAuth;
+    FirebaseUser fUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savings);
 
-        imgBtn = findViewById(R.id.imgBtn_saving);
+        setUpToolbar();
 
+        navigationView = (NavigationView) findViewById(R.id.navigation_menu2);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case  R.id.nav_aboutus:
+
+                        Intent intent = new Intent(Savings.this, about_page.class);
+                        startActivity(intent);
+                        break;
+
+                    case  R.id.nav_notification2:
+
+                        Intent intent1 = new Intent(Savings.this, Notification.class);
+                        startActivity(intent1);
+
+                        break;
+
+                    case  R.id.nav_tips:
+
+                        Intent intent2 = new Intent(Savings.this, QuitSmokingTips.class);
+                        startActivity(intent2);
+
+                        break;
+
+                    case R.id.nav_healthprogress:
+                        Intent intent3 = new Intent(Savings.this, HealthProgress.class);
+                        startActivity(intent3);
+
+                        break;
+
+                    case R.id.nav_savings:
+                        Intent intent4 = new Intent(Savings.this, Savings.class);
+                        startActivity(intent4);
+
+                        break;
+
+                    case R.id.nav_products:
+                        Intent intent5 = new Intent(Savings.this, ShowProducts.class);
+                        startActivity(intent5);
+
+                        break;
+
+                    case R.id.nav_logout2:
+
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(Savings.this, login_page.class));
+
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+        imgBtn = findViewById(R.id.imgBtn_saving);
         imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -104,6 +174,115 @@ public class Savings extends AppCompatActivity {
         savingArray.add(saving);
         saving = new SavingHistory("24/11/2021","Successfully Avoid Smoking","RM 15.00");
         savingArray.add(saving);
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    public void setUpToolbar() {
+        drawerLayout = findViewById(R.id.drawer_Layout);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+        alertDlg.setMessage("Are you sure want to exit");
+        alertDlg.setCancelable(false);
+        alertDlg.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent a = new Intent(Intent.ACTION_MAIN);
+                a.addCategory(Intent.CATEGORY_HOME);
+                a.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(a);
+
+            }
+        });
+        alertDlg.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertDlg.create().show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile){
+            Intent intent5 = new Intent(Savings.this, smoker_profile.class);
+            startActivity(intent5);
+            return true;
+        }
+
+        else if(id == R.id.nav_changepwrd){
+            Intent intent2 = new Intent(Savings.this, Change_password.class);
+            startActivity(intent2);
+            return true;
+        }
+
+        switch (item.getItemId()){
+
+            case R.id.nav_notification:
+                Intent intent = new Intent(Savings.this, Notification.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_profile:
+                Intent intent5 = new Intent(Savings.this, smoker_profile.class);
+                startActivity(intent5);
+                break;
+
+            case R.id.nav_feedback:
+                Intent intent1 = new Intent(Savings.this, user_feedback_page.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.nav_changepwrd:
+                Intent intent2 = new Intent(Savings.this, Change_password.class);
+                startActivity(intent2);
+                break;
+
+            /*case R.id.nav_setting:
+                Intent intent3 = new Intent(Savings.this, Setting.class);
+                startActivity(intent3);
+                break;*/
+
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Savings.this, login_page.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        fAuth=FirebaseAuth.getInstance();
+        fUser=fAuth.getCurrentUser();
+
+        if(fUser==null){
+            //go to login page
+            Intent intent = new Intent(Savings.this,login_page.class);
+            startActivity(intent);
+        }
+
     }
 
 
