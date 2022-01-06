b5d6555378dc4bf02e9dcb1b37f5674b5a2d56d7
azwahnfi <azwa_hanafiah@yahoo.com>
2022-01-07 02:26:33 +0800

    notification

--- app/src/main/java/com/khr/justquitit/Notification.java
+++ app/src/main/java/com/khr/justquitit/Notification.java
@@ -1,13 +1,29 @@
 package com.khr.justquitit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Notification extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("justquitit")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Done";
+                        if(!task.isSuccessful()){
+                            msg = "Failed";
+                        }
+                    }
+                });
     }
 }
